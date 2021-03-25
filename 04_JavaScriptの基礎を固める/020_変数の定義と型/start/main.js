16381cf787b67fd6265bf393c7aff6a0b6bd62d4
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-03-25 21:34:19 +0900

    update

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,17 @@
+
+let name = 'Tom';
+name = "Tim"
+console.log('hello ' + name);
+
+
+
+/* 動的型付け言語 */
+
+//Number,String, Boolean,Undefined, Null, Symbol
+
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+console.log(typeof variable);
+
