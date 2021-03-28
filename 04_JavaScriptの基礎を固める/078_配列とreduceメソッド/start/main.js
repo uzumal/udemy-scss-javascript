c1b99bfed0582d91a5a392c69f26de2228600930
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-03-28 11:49:45 +0900

    Update

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,19 @@
-const arry = [1,2,3,4,5];
+const arry = [1,2,6,4,5];
+
+arry.reduce(function(accu, curr) {
+    console.log(accu,curr);
+    // return accu + curr;
+});
+
+const str = 'animation';
+const strArry = str.split('');
+
+console.log(strArry);
+
+/* "" 空文字を入れることで配列一番目を参照させる */
+const result = strArry.reduce((accu, curr) => {
+    // return accu + "<" + curr + ">";
+    return `${accu}<${curr}>`;
+}, "");
+
+console.log(result);
